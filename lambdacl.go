79f352616bbeb5066db70d264c32ef18df546a2b
maxmcd <max.t.mcdonnell@gmail.com>
2015-02-14 17:59:15 -0500

    working lambda creation, execution and partial log retrieval

--- lambdacl.go
+++ lambdacl.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"os"
 	"os/exec"
+	"regexp"
+	"time"
 )
 
 func main() {
 
-	code := `
-    console.log('Loading event');
-    exports.handler = function(event, context) {
-      console.log("value1 = " + event.key1);
-      console.log("value2 = " + event.key2);
-      console.log("value3 = " + event.key3);
-      context.done(null, "Hello World");  // SUCCESS with message
-    }`
+	// code := `
+	//    console.log('Loading event');
+	//    exports.handler = function(event, context) {
+	//      console.log("value1 = " + event.key1);
+	//      console.log("value2 = " + event.key2);
+	//      console.log("value3 = " + event.key3);
+	//      context.done(null, "Hello World");  // SUCCESS with message
+	//    }`
 
-	create("helloworld2", code)
+	// err := create("helloworld3", code)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	err := run("helloworld3")
+	if err != nil {
+		panic(err)
+	}
+	getStats("helloworld3")
+}
 
-	run()
+func randSeq(n int) string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyz1234567890-")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
 
 // {
@@ -28,46 +51,151 @@ func main() {
 // }
 
 func create(functionName, code string) (err error) {
-	filename := createZip(code)
+	filename, err := createZip(code)
+	if err != nil {
+		return
+	}
 	cmd := exec.Command(
 		"aws",
 		"lambda",
 		"upload-function",
 		"--region", "us-east-1",
 		"--function-name", functionName,
-		"--function-zip", "file.zip",
+		"--function-zip", filename,
 		"--role", "arn:aws:iam::651778473396:role/lambda_s3_role",
 		"--mode", "event",
-		"--handler", "helloworld.handler",
+		"--handler", "index.handler",
 		"--runtime", "nodejs",
 	)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 
-	cmd.Run()
+	// cmd.Run()
 
-	cmd.Wait()
+	// cmd.Wait()
+	return
 }
 
-func createZip(code string) (filename string) {
+func createZip(code string) (filename string, err error) {
+	buf := new(bytes.Buffer)
+	w := zip.NewWriter(buf)
 
+	var files = []struct {
+		Name, Body string
+	}{
+		{"index.js", code},
+	}
+
+	for _, file := range files {
+		f, err := w.Create(file.Name)
+		if err != nil {
+			return filename, err
+		}
+		_, err = f.Write([]byte(file.Body))
+		if err != nil {
+			return filename, err
+		}
+	}
+
+	err = w.Close()
+	if err != nil {
+		return filename, err
+	}
+
+	filename = randSeq(10) + ".zip"
+	err = ioutil.WriteFile(filename, buf.Bytes(), 0644)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
-func run() {
+func run(functionName string) (err error) {
 	cmd := exec.Command(
 		"aws",
 		"lambda",
 		"invoke-async",
 		"--region", "us-east-1",
-		"--function-name", "helloworld",
+		"--function-name", functionName,
 		"--invoke-args", "inputfile.txt",
+		"--debug",
 	)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	// cmd.Stdin = os.Stdin
+	re := regexp.MustCompile("'x-amzn-requestid': '(.*?)'")
+
+	var b bytes.Buffer
+	var e bytes.Buffer
+	cmd.Stdout = &b
+	cmd.Stderr = &e
+	err = cmd.Run()
+	debugOutput := e.Bytes()
+	if err != nil {
+		return
+	}
+	resultString := re.FindStringSubmatch(string(debugOutput))
+	if len(resultString) > 1 {
+		fmt.Println(resultString[1])
+	} else {
+		err = fmt.Errorf("Request id not found")
+	}
+	fmt.Println(string(b.Bytes()))
 
 	cmd.Run()
 
 	cmd.Wait()
+	return
+}
+
+func getStats(functionName string) {
+	cmd := exec.Command(
+		"aws",
+		"logs",
+		"describe-log-streams",
+		"--region", "us-east-1",
+		"--log-group-name", "/aws/lambda/"+functionName,
+	)
+
+	time.Sleep(time.Second * 3)
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	// cmd.Stdin = os.Stdin
+	jsonOutput, err := cmd.CombinedOutput()
+	// fmt.Println(string(jsonOutput))
+	// cmd.Run()
+
+	// cmd.Wait()
+	f := make(map[string][]map[string]interface{})
+	err = json.Unmarshal(jsonOutput, &f)
+
+	var largestTimestamp float64
+	largestTimestamp = 0
+	largestIndex := 0
+	for index, value := range f["logStreams"] {
+		if value["lastEventTimestamp"].(float64) > largestTimestamp {
+			largestTimestamp = value["lastEventTimestamp"].(float64)
+			largestIndex = index
+		}
+	}
+	logStreamName := f["logStreams"][largestIndex]["logStreamName"].(string)
+
+	// panic(err)
+	if err != nil {
+		panic(err)
+	}
+
+	// aws logs get-log-events --log-group-name /aws/lambda/helloworld3 --log-stream-name 0aee4e0328f341599460d14a2a1e6b53 --region us-east-1
+	cmd = exec.Command(
+		"aws",
+		"logs",
+		"get-log-events",
+		"--region", "us-east-1",
+		"--log-stream-name", logStreamName,
+		"--log-group-name", "/aws/lambda/"+functionName,
+	)
+	newOut, err := cmd.Output()
+	fmt.Println(string(newOut))
 }
--- main.go
+++ main.go
@@ -5,11 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 
-	"github.com/awslabs/aws-sdk-go/aws"
-	"github.com/awslabs/aws-sdk-go/service/lambda"
-	goaws "github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/s3"
 )
 
@@ -26,62 +23,6 @@ func main() {
 	// http.HandleFunc("/lambda/new", lambdaNewHandler)
 	// http.HandleFunc("/lambda/trigger", lambdaTriggerHandler)
 	// http.ListenAndServe(":8080", nil)
-}
-
-func lambdaTest() {
-	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
-	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-	creds := aws.Creds(accessKey, secretKey, "")
-	config := aws.Config{
-		Credentials: creds,
-		Region:      "us-west-2",
-	}
-	newLambda := lambda.New(&lambda.LambdaConfig{&config})
-	// newLambda.InvokeAsync(req)
-
-	fileBody := `
-	console.log('Loading event');
-	var aws = require('aws-sdk');
-	var s3 = new aws.S3({apiVersion: '2006-03-01'});
-
-	exports.handler = function(event, context) {
-	   console.log('Received event:');
-	   console.log(JSON.stringify(event, null, '  '));
-	   // Get the object from the event and show its content type
-	   var bucket = event.Records[0].s3.bucket.name;
-	   var key = event.Records[0].s3.object.key;
-	   s3.getObject({Bucket:bucket, Key:key},
-	      function(err,data) {
-	        if (err) {
-	           console.log('error getting object ' + key + ' from bucket ' + bucket + 
-	               '. Make sure they exist and your bucket is in the same region as this function.');
-	           context.done('error','error getting file'+err);
-	        }
-	        else {
-	           console.log('CONTENT TYPE:',data.ContentType);
-	           context.done(null,'');
-	        }
-	      }
-	   );
-	};
-	`
-
-	lambda.U
-	req := lambda.UploadFunctionRequest{
-		Description:  aws.String("This is a test"),
-		FunctionName: aws.String("testFunction"),
-		FunctionZip:  []byte(fileBody),
-		Handler:      aws.String("handler"),
-		MemorySize:   aws.Integer(128),
-		Role:         aws.String("lambda_s3_role"),
-		Runtime:      aws.String("nodejs"),
-		Timeout:      aws.Integer(30),
-	}
-	resp, err := newLambda.UploadFunction(&req)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(resp)
 }
 
 func lambdaTriggerHandler(w http.ResponseWriter, r *http.Request) {
@@ -255,11 +196,11 @@ func getParseUsernameFromSession(session string) (username string, err error) {
 
 func uploadS3File(username string, contents []byte, fileType string, key string) (err error) {
 	// grabs auth values from env variables
-	auth, err := goaws.EnvAuth()
+	auth, err := aws.EnvAuth()
 	if err != nil {
 		return
 	}
-	client := s3.New(auth, goaws.USEast)
+	client := s3.New(auth, aws.USEast)
 	bucket := client.Bucket("romulus-host")
 	err = bucket.Put(username+"/"+key, contents, fileType, s3.PublicRead)
 	return
